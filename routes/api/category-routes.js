b1dd6dd40ae60bb6da176a7f744a5b3f3018771c
Joaquin Salazar <joaqsala@gmail.com>
2023-05-08 22:38:00 -0500

    added post route for categories model

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,18 +23,26 @@ router.get('/:id', async (req, res) => {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
   });
+
   if (!categoryData) {
     res.status(404).json({ message: 'No category found with that id!' });
     return;
   }
+
   res.status(200).json(categoryData);
 } catch (err) {
   res.status(500).json(err);
 }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {

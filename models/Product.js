c19bcd83a658ff347c2d0579b5982b8b803ac824
Joaquin Salazar <joaqsala@gmail.com>
2023-05-08 22:22:19 -0500

    added comments in models

--- models/Product.js
+++ models/Product.js
@@ -9,6 +9,7 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+     //define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -30,7 +31,10 @@ Product.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
+      validate: {
+        isNumeric: true,
     },
+  },
     category_id: {
       type: DataTypes.INTEGER,
       // This references the `category` model, which was set in `Category.js` in the`modelName` property
